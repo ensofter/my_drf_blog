b708dc410d5033ca96ffe9bdb98fe80daf79cf82
ensofter <bogestvenniyiri@yandex.ru>
2020-12-30 03:09:42 +0300

    Change search query param

--- api/api/settings.py
+++ api/api/settings.py
@@ -107,6 +107,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
+    "SEARCH_PARAM": "q"
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
